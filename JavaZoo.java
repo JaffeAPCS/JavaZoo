5a305d4352e41dae7cd1ac64b6d8124589709422
Roger <rogerjaffe@gmail.com>
2018-09-17 08:16:15 -0700

    Added instantiation for classes defined by strings

--- JavaZoo.java
+++ JavaZoo.java
@@ -14,15 +14,21 @@ public class JavaZoo
     // method to test your class.  You can also test your class using BlueJ
     String[] animalList = {"RogerJaffe"};
     
+    int delay = 4;
+    
     try {
       for (int i=0; i<animalList.length; i++) {
+        //Class<?> c = Class.forName(animalList[i]);
+        //Animal a = (Animal)c.newInstance();
         Animal a = new RogerJaffe();
         System.out.println("Animal #"+(i+1)+" by "+a.getClassName());
         System.out.println(a.toString());
         System.out.println();
+        TimeUnit.SECONDS.sleep(delay);
       }
     } catch (Exception e) {
       System.out.println("Someone messed up!");
+      System.out.println(e.getMessage());
     }
   }
 }
