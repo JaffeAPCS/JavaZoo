e4b77538103bfb5ed3e473f81e9ed1896243c029
Roger <rogerjaffe@gmail.com>
2018-09-18 08:09:11 -0700

    Commenting changes

--- JavaZoo.java
+++ JavaZoo.java
@@ -14,13 +14,17 @@ public class JavaZoo
     // method to test your class.  You can also test your class using BlueJ
     String[] animalList = {"RogerJaffe"};
     
-    int delay = 4;
+    int delay = 5;
     
     try {
       for (int i=0; i<animalList.length; i++) {
+        // Change the name to the name of your class to test
+        Animal a = new RogerJaffe();
+        
+        // These lines will be used to run the program with all the student classes
         //Class<?> c = Class.forName(animalList[i]);
         //Animal a = (Animal)c.newInstance();
-        Animal a = new RogerJaffe();
+        
         System.out.println("Animal #"+(i+1)+" by "+a.getClassName());
         System.out.println(a.toString());
         System.out.println();
